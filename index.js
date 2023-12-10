51bff114570a4c4c0f994020104ce0a730e1edaf
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-10 04:50:44 -0500

    restructured index and made shapes.js

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
-const inquirer = require('inquirer')
-const svgBuilder = require('svg-builder')
-const fs = require('fs')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
-async function generateLogo() {
-    const { text } = await inquirer.prompt({
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters'
-    })
+async function promptUser() {
+  const userInput = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (value) => value.length <= 3,
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal):',
+    },
+  ]);
 
-    const { textColor } = await inquirer.prompt({
-        type: 'input',
-        name: 'textColor',
-        message: 'Ender the text color (color keyword or hexadecimal)'
-    })
-
-    const shapes = ['circle', 'triangle', 'square']
-    const { selectedShapes } = await inquirer.prompt({
-        type: 'list',
-        name: 'selectedShapes',
-        message: 'Choose a shape',
-        choices: shapes
-    })
-
-    const { shapeColor } = await inquirer.prompt({
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter shape color (color keyword or hexadecimal)'
-    })
-
-    const svgContent = svgBuilder({
-        width: 300,
-        height: 200,
-        elements: [
-            { type: 'text', content: text, x:150, y: 150, fill: textColor },
-            { type: selectedShapes, x:150, y:150, size:50, fill, shapeColor }
-        ]
-    })
-
-    fs.writeFileSync('dist/logo.svg', svgContent)
-
-    console.log('Generated logo.svg')
+  return userInput
 }
 
-generateLogo()
+function generateSVG(text, textColor, shape, shapeColor) {
+  let svgElement
+
+  switch (shape) {
+    case 'circle':
+      svgElement = new Circle(text, textColor, shapeColor);
+      break;
+    case 'triangle':
+      svgElement = new Triangle(text, textColor, shapeColor);
+      break;
+    case 'square':
+      svgElement = new Square(text, textColor, shapeColor);
+      break;
+    default:
+      throw new Error('Invalid shape')
+  }
+
+  const svgContent = svgElement.getSVG()
+  fs.writeFileSync('dist/logo.svg', svgContent)
+
+  console.log('Generated logo.svg')
+}
+
+async function run() {
+  const userInput = await promptUser()
+  generateSVG(userInput.text, userInput.textColor, userInput.shape, userInput.shapeColor)
+}
+
+run()
