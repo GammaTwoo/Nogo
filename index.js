588166267af84ce6a1b47032831a9f7a20ebf6e9
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-10 03:36:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const inquirer = require('inquirer')
+const svgBuilder = require('svg-builder')
+const fs = require('fs')
---
+++ tests/index.test.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+const generateLogo = require('../index')
