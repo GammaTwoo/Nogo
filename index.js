9e19b9606742c36ca09489f85d7a33586f50f436
GammaTwoo <gavin.frazier63@gmail.com>
2023-12-10 03:49:30 -0500

    questions complete

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const inquirer = require('inquirer')
 const svgBuilder = require('svg-builder')
 const fs = require('fs')
+
+async function generateLogo() {
+    const { text } = await inquirer.prompt({
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters'
+    })
+
+    const { textColor } = await inquirer.prompt({
+        type: 'input',
+        name: 'textColor',
+        message: 'Ender the text color (color keyword or hexadecimal)'
+    })
+
+    const shapes = ['circle', 'triangle', 'square']
+    const { selectedShapes } = await inquirer.prompt({
+        type: 'list',
+        name: 'selectedShapes',
+        message: 'Choose a shape',
+        choices: shapes
+    })
+
+    const { shapeColor } = await inquirer.prompt({
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (color keyword or hexadecimal)'
+    })
+}
